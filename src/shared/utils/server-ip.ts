41a38034393ccd10017807e648445b586949ee76
尹春阳 <2646403766@qq.com>
2025-04-05 19:29:36 +0800

    feat: 环境变量

--- src/shared/utils/server-ip.ts
+++ src/shared/utils/server-ip.ts
@@ -1,17 +1,41 @@
+// // 获取本机 IPv4 地址（排除内部网络）
+// import * as os from 'node:os';
+//
+// export function getServerIp(): string {
+//   const port = process.env.PORT;
+//   const interfaces = os.networkInterfaces();
+//   for (const interfaceName of Object.keys(interfaces)) {
+//     const interfaceInfo = interfaces[interfaceName];
+//     if (!interfaceInfo) return `http://localhost:${port}`;
+//     for (const iface of interfaceInfo) {
+//       if (iface.family === 'IPv4' && !iface.internal) {
+//         return `http://${iface.address}:${port}`;
+//       }
+//     }
+//   }
+//   return `http://localhost:${port}`;
+// }
 // 获取本机 IPv4 地址（排除内部网络）
 import * as os from 'node:os';
 
 export function getServerIp(): string {
-  const port = process.env.PORT;
-  const interfaces = os.networkInterfaces();
-  for (const interfaceName of Object.keys(interfaces)) {
-    const interfaceInfo = interfaces[interfaceName];
-    if (!interfaceInfo) return `http://localhost:${port}`;
-    for (const iface of interfaceInfo) {
-      if (iface.family === 'IPv4' && !iface.internal) {
-        return `http://${iface.address}:${port}`;
-      }
-    }
-  }
-  return `http://localhost:${port}`;
+  const port = process.env.UPLOAD_PORT;
+  const upload_ip = process.env.UPLOAD_IP;
+  // const interfaces = os.networkInterfaces();
+  // for (const interfaceName of Object.keys(interfaces)) {
+  //   const interfaceInfo = interfaces[interfaceName];
+  //   if (!interfaceInfo) return `http://localhost:${port}`;
+  //   for (const iface of interfaceInfo) {
+  //     if (iface.family === 'IPv4' && !iface.internal) {
+  //       return `http://${iface.address}:${port}`;
+  //     }
+  //   }
+  // }
+  // let ip = `localhost`
+  // if(upload_ip){
+  //   ip = upload_ip
+  // }
+  // return `http://${ip}:${port}`;
+
+  return `http://${upload_ip}:${port}`;
 }
